f96c00ea615a4e664611ced7befc11f30ec0ed38
toni714 <tonilupin@aol.de>
2017-07-21 20:37:28 +0200

    Parsing All Videos
    Extension is now parsing youtube pages for videos and prints their title via console.log()

--- extension/js/blocking_script.js
+++ extension/js/blocking_script.js
@@ -1,28 +1,30 @@
 var blocked_users=getBlockedUsersFromBrowser();
 var videos=getVideosFromDOM();
-for video in videos{
-  video.print_self()
-  if blocked_users.contains(video.user){
+
+videos.forEach(processVideo);
+
+function processVideo(video, index){
+  video.print_self();
+  if (blocked_users.contains(video.user)) {
     video.user.block_video(video);
   }
 }
 
-function getBlockedUsersFromBackground(){
-  browser.storage.local.get("blocked_users");
-  //return browser.runtime.sendMessage("GET USERS");
+function getBlockedUsersFromBrowser(){
+  return browser.storage.local.get("blocked_users");
 }
 
-function getVideosfromDOM(){
+function getVideosFromDOM(){
   var dom_videos=document.getElementsByClassName("yt-shelf-grid-item");
   var videos=[];
-  for dom_video in dom_videos{
-    videos.add(new Video(dom_video));
+  for(var i=0;i<dom_videos.length;i++){
+    videos.push(new Video(dom_videos[i]));
   }
   return videos;
 }
 
 function Video(dom_video){
   this.dom_video=dom_video;
-  this.title=dom_video.getChildrenByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
+  this.title=dom_video.getElementsByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
     console.log(this.title)
 }
