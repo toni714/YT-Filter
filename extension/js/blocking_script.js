41671c23308c7ada821a7fbf87a4b3384687a2b6
toni714 <tonilupin@aol.de>
2017-07-21 21:37:47 +0200

    Rewriting Blocking System
    Rewriting of how videos should be blocked in progress.
    dosn't work right now!

--- extension/js/blocking_script.js
+++ extension/js/blocking_script.js
@@ -1,49 +1,53 @@
 
 var blocked_users=getBlockedUsersFromBrowser();
-var videos=getVideosFromDOM();
+var blocked_dict=generateBlockedDict(blocked_users);
+getVideosFromDOM(blocked_dict);
 
+for(var username in blocked_dict){
+  if(blocked_dict.hasOwnProperty(username)){
+    blocked_dict[username].forEach(function (video, index){
+      //TODO block this video like instructed
+    });
+  }
+}
 videos.forEach(processVideo);
 
-function processVideo(video, index){
-  video.print_self();
-  /*if (blocked_users.contains(video.user)) {
-    video.user.block_video(video);
-  }*/
+function getBlockedUsersFromBrowser(blocked_users){
+  ret={};
+  blocked_users.forEach(function (user, index){
+    ret[user.name]=[];
+  });
+  return ret;
 }
 
 function getBlockedUsersFromBrowser(){
   return browser.storage.local.get("blocked_users");
 }
 
-function getVideosFromDOM(){
+function getVideosFromDOM(blocked_dict){
   var dom_videos=null
   var onWatchpage=window.location.href.indexOf("watch?v=")!==-1
   if (onWatchpage) {
-    console.log("watchpage")
     dom_videos=document.getElementsByClassName("video-list-item");
   }else{
-    console.log("otherpage")
     dom_videos=document.getElementsByClassName("yt-shelf-grid-item");
   }
-  var videos=[];
+
   for(var i=0;i<dom_videos.length;i++){
-    console.log(dom_videos[i]);
-    videos.push(new Video(dom_videos[i], onWatchpage));
+    var vid=new Video(dom_videos[i], onWatchpage)
+    blocked_dict[vid.username].push(vid);
   }
-  return videos;
 }
 
 function Video(dom_video, onWatchpage){
   this.dom_video=dom_video;
   if(onWatchpage){
-
     this.title=dom_video.getElementsByClassName("content-link")[0].getAttribute("title");
-    console.log(this.title);
+    //recommended video
   }else{
+    //from homepage or other
     this.title=dom_video.getElementsByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
-    console.log(this.title);
   }
-  //console.log(this.title)
   this.print_self=function (){
     console.log(this.title);
   }
