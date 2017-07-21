5d725f43f763b35de0286b136777653c1442a3d7
toni714 <tonilupin@aol.de>
2017-07-22 00:28:37 +0200

    Filtering Implemented
    JS now correctly filters contents internally, they have title, username, and DOM object members

--- extension/js/blocking_script.js
+++ extension/js/blocking_script.js
@@ -1,26 +1,29 @@
+getBlockedUsersFromBrowser().then(continue_blocking);
 
-var blocked_users=getBlockedUsersFromBrowser();
-var blocked_dict=generateBlockedDict(blocked_users);
-getVideosFromDOM(blocked_dict);
 
-for(var username in blocked_dict){
-  if(blocked_dict.hasOwnProperty(username)){
-    blocked_dict[username].forEach(function (video, index){
-      //TODO block this video like instructed
-    });
-  }
-}
-videos.forEach(processVideo);
 
-function getBlockedUsersFromBrowser(blocked_users){
-  ret={};
-  blocked_users.forEach(function (user, index){
-    ret[user.name]=[];
+function continue_blocking(users){
+  var blocked_dict={};
+  console.log(users["blocked_users"]);
+  users["blocked_users"].forEach(function (user, index){
+    //TODO change to user.name
+    blocked_dict[user]=[];
   });
-  return ret;
+  getVideosFromDOM(blocked_dict)
+
+  console.log(blocked_dict);
+  for(var username in blocked_dict){
+    if(blocked_dict.hasOwnProperty(username)){
+      blocked_dict[username].forEach(function (video, index){
+        //TODO block this video like instructed
+        console.log(video);
+      });
+    }
+  }
 }
 
 function getBlockedUsersFromBrowser(){
+  browser.storage.local.set({blocked_users:["test", "Galileo", "Zombey"]});
   return browser.storage.local.get("blocked_users");
 }
 
@@ -35,20 +38,28 @@ function getVideosFromDOM(blocked_dict){
 
   for(var i=0;i<dom_videos.length;i++){
     var vid=new Video(dom_videos[i], onWatchpage)
-    blocked_dict[vid.username].push(vid);
+    //vid.print_self();
+    if(blocked_dict.hasOwnProperty(vid.username)){
+      blocked_dict[vid.username].push(vid);
+    }
   }
 }
 
 function Video(dom_video, onWatchpage){
   this.dom_video=dom_video;
+  //TODO add username
   if(onWatchpage){
     this.title=dom_video.getElementsByClassName("content-link")[0].getAttribute("title");
+    this.username=dom_video.getElementsByClassName("g-hovercard")[0].innerHTML;
     //recommended video
   }else{
     //from homepage or other
+    //TODO differentiate search and homepage
     this.title=dom_video.getElementsByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
+    this.username=dom_video.getElementsByClassName("g-hovercard")[0].innerHTML;
   }
   this.print_self=function (){
-    console.log(this.title);
+    console.log("TITLE: "+this.title+" USERNAME: "+this.username);
   }
+  //TODO add blocking option
 }
