1b1d141821c88dd7281e1bd957346055b12703e8
toni714 <tonilupin@aol.de>
2017-07-21 21:06:48 +0200

    Include Recommended
    Parsing now also works with recommended videos on */watch?v=*

--- extension/js/blocking_script.js
+++ extension/js/blocking_script.js
@@ -1,3 +1,4 @@
+
 var blocked_users=getBlockedUsersFromBrowser();
 var videos=getVideosFromDOM();
 
@@ -5,9 +6,9 @@ videos.forEach(processVideo);
 
 function processVideo(video, index){
   video.print_self();
-  if (blocked_users.contains(video.user)) {
+  /*if (blocked_users.contains(video.user)) {
     video.user.block_video(video);
-  }
+  }*/
 }
 
 function getBlockedUsersFromBrowser(){
@@ -15,16 +16,35 @@ function getBlockedUsersFromBrowser(){
 }
 
 function getVideosFromDOM(){
-  var dom_videos=document.getElementsByClassName("yt-shelf-grid-item");
+  var dom_videos=null
+  var onWatchpage=window.location.href.indexOf("watch?v=")!==-1
+  if (onWatchpage) {
+    console.log("watchpage")
+    dom_videos=document.getElementsByClassName("video-list-item");
+  }else{
+    console.log("otherpage")
+    dom_videos=document.getElementsByClassName("yt-shelf-grid-item");
+  }
   var videos=[];
   for(var i=0;i<dom_videos.length;i++){
-    videos.push(new Video(dom_videos[i]));
+    console.log(dom_videos[i]);
+    videos.push(new Video(dom_videos[i], onWatchpage));
   }
   return videos;
 }
 
-function Video(dom_video){
+function Video(dom_video, onWatchpage){
   this.dom_video=dom_video;
-  this.title=dom_video.getElementsByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
-    console.log(this.title)
+  if(onWatchpage){
+
+    this.title=dom_video.getElementsByClassName("content-link")[0].getAttribute("title");
+    console.log(this.title);
+  }else{
+    this.title=dom_video.getElementsByClassName("yt-lockup-title")[0].children[0].getAttribute("title");
+    console.log(this.title);
+  }
+  //console.log(this.title)
+  this.print_self=function (){
+    console.log(this.title);
+  }
 }
